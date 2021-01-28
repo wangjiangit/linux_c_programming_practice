e61a67a8770e0cc876df8505dc984524a66df443
wangjian <jiji_12342006@163.com>
2021-01-28 10:09:34 +0800

    3-3~3-5

---
+++ chapter_3/3-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0, i = 1;
+    do {
+        sum = sum + i;
+        i = i + 1;
+    } while (i <= 100)
+    printf("%d\n", sum);
+}
---
+++ chapter_3/3-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    for (number = 1; number < = 100; number++) {
+        if (number % 13 == 0)
+            continue;
+        printf("%d,number");
+    }
+
+}
