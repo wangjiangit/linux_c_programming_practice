e4e5a4b2f0e6b0a02fe87ed4a9a6bf219fd8426e
汪见 <jiji_12342006@163.com>
2021-01-30 20:21:14 +0800

    3-6~3-7 gcc 编译器知识

---
+++ chapter_3/3-7.c
@@ -0,0 +1,25 @@
+/*
+ 1:
+ for(i=0;i<N;i++)
+ {
+    if(condition){
+        doSomething();
+    }else{
+        doOtherSomething();
+    }
+ }
+
+ 2:
+  if(condition){
+    for(i=0;i<N;i++){
+     doSomething();
+    }
+  }else{
+     for(i=0;i<N;i++){
+     doOthering();
+    }
+
+  }
+
+ */
+
